45e941e061d8b1ea706c8ae90eb4f209ca97dc89
kaio alves <kaio.alves19@gmail.com>
2024-11-22 10:00:54 -0300

    Implement content management features with HTML form, JavaScript API integration, and UML class diagram

--- src/content_app/views.py
+++ src/content_app/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Content
 from .serializers import ContentSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class ContentViewSet(viewsets.ModelViewSet):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
